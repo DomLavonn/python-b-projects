80d08daf5281bb66f5e9730f7bd77e72b359eaad
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-07-29 17:37:21 -0400

    updates game

--- tictactoe/Game.py
+++ tictactoe/Game.py
@@ -10,14 +10,17 @@ class Game:
         print("Welcome to tictactoe!\n\n")
         while(1):
             pick = input("Enter x or o.\n")
-            board_index = ("Enter the index of arr")
+            board_index = input("Enter the index of arr")
             self.show_board(self.board, pick, board_index)
+            
     
     
-    def show_board(self, board, pick):
+    def show_board(self, board, pick, board_index):
         
         for i in board:
             print(i)
+                
+                
                 
     def tictac(self):
         
